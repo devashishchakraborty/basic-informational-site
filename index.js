99b1ef2c8e6d33e1b157856698c62ae7444d2f11
Devashish Chakraborty <devashishchakra@gmail.com>
2025-01-05 20:26:40 +0530

    Completed the project

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-console.log("Hello World!");
+import { createServer } from "http";
+import { parse } from "url";
+import { readFile } from "fs";
+
+createServer((req, res) => {
+  const q = parse(req.url, true);
+  const filename = "." + (q.pathname === "/" ? "/index.html" : q.pathname);
+  readFile(filename, (err, data) => {
+    if (err) {
+      readFile("./404.html", (err, data) => {
+        if (err) {
+          res.writeHead(404, {"Content-Type": "text/html"});
+          return res.end("404: Not Found");
+        }
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write(data);
+        return res.end();
+      })
+    }
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(data);
+    return res.end();
+  })
+}).listen(8080);
+
+console.log("Running on http://localhost:8080/")
