3cb8597483ff21f7351be0e60be82b22b866440f
Devashish Chakraborty <devashishchakra@gmail.com>
2025-01-05 21:07:21 +0530

    Fixed error related to 404 pages

--- index.js
+++ index.js
@@ -4,23 +4,24 @@ import { readFile } from "fs";
 
 createServer((req, res) => {
   const q = parse(req.url, true);
-  const filename = "." + (q.pathname === "/" ? "/index.html" : q.pathname);
+  const pages = ["/index.html", "/about.html", "/contact-me.html"];
+  const filename =
+    "." +
+    (pages.includes(q.pathname)
+      ? q.pathname
+      : q.pathname === "/"
+      ? "/index.html"
+      : "/404.html");
+
   readFile(filename, (err, data) => {
     if (err) {
-      readFile("./404.html", (err, data) => {
-        if (err) {
-          res.writeHead(404, {"Content-Type": "text/html"});
-          return res.end("404: Not Found");
-        }
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        res.write(data);
-        return res.end();
-      })
+      res.writeHead(404, { "Content-Type": "text/html" });
+      return res.end("404: Not Found");
     }
-    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.writeHead(200, { "Content-Type": "text/html" });
     res.write(data);
     return res.end();
-  })
+  });
 }).listen(8080);
 
-console.log("Running on http://localhost:8080/")
+console.log("Running on http://localhost:8080/");
